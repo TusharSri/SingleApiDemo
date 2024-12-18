da492b7bc59f742c3a2aaec8a0acb1c71fd9774e
Tushar Srivastava <tusharsri90@gmail.com>
2024-12-18 14:45:32 +0530

    Initial commit for single api

---
+++ src/main/java/com/employee/SingleApiDemo/SingleApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.employee.SingleApiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee/SingleApiDemo/SingleApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.SingleApiDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
